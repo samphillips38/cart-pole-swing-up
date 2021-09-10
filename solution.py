02d38e23835b0be35f33df961c4d95a96db0cf3f
Sam Phillips <srp60@cam.ac.uk>
2021-09-10 13:30:32 +0100

    feat: function to calculate system dynamics

--- solution.py
+++ solution.py
@@ -2,3 +2,18 @@
 
 import numpy as np
 
+# Problem global paramters
+m1 = 10
+m2 = 10
+l = 10
+g = 9.81
+
+# Dynamics
+def x_dot(x, u):
+    """Calculate the dynamics given the state, x and the control, u"""
+    [_, q2, q1_dot, q2_dot] = x # [horzontal_position, angle, ...]
+
+    q1_ddot = (l*m1*np.sin(q2)*q2_dot**2 + u + m2*g*np.cos(q2)*np.sin(q2)) / (m1 + m2*(1 - np.cos(q2)**2))
+    q2_ddot = (l*m2*np.cos(q2)*np.sin(q2)*q2_dot**2 + u*np.cos(q2) + (m1 + m2)*g*np.sin(q2)) / (l*m1 + l*m2*(1 - np.cos(q2)**2))
+
+    return np.array([q1_dot, q2_dot, q1_ddot, q2_ddot])
